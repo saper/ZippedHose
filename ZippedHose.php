327d07683021659ff5bcb3795b263fd22b9010e2
saper <saper@saper.info>
2015-03-10 23:08:01 +0100

    Unzip -l works
    This is a very simple handler to stream
    a collection of files as the generated ZIP
    file.

---
+++ ZippedHose.php
@@ -0,0 +1,59 @@
+<?php
+
+function statone($f) {
+	 stat($f);
+	$s['filename'] = $f;
+	print_r($s);
+	return $s;
+}
+
+function statfiles($filelist) {
+	return array_map(null, $filelist, array_map('stat', $filelist));
+}
+
+function coreheader($fstat, $extra) {
+	return pack("v5V3v2",
+			10, /* 20 for dir, 45 for zip64 */
+			0x0000,    /* special purpose */
+			0x0000,    /* no compression */
+			0x0000,    /* mod time  */
+			0x0000,    /* mod date  */
+			0x00000000,        /* CRC32 */
+			0x00000000,        /* compressed size */
+			0x00000000,        /* uncompressed size */
+			strlen($fstat[0]), /* file name length */
+			strlen($extra));   /* extra field length */
+}
+
+function localheader($fstat, $extra) {
+	return pack("V", 0x04034b50) .
+		coreheader($fstat, $extra) .
+		$fstat[0] .
+		$extra;
+}
+
+function centralheader($fstat, $extra) {
+	return pack("Vv",
+			0x02014b50,
+			0x0000) . /* Version */
+		coreheader($fstat, $extra) .
+		pack("v3V2",
+			0x0000,    /* file comment length */
+			0x0000,    /* disk number start */
+			0x0000,    /* internal file attributes */
+			0x0000,    /* external file attributes */
+			0x00000000) .      /* localheader relative offset */
+		$fstat[0] .
+		$extra;
+}
+function endcentral($centraloffset, $centralsize, $entries) {
+	return pack("Vv4V2v",
+			0x06054b50,
+			0x0000,    /* number of this disk */
+			0x0000,    /* disk where central starts */
+			$entries,  /* number of disk with central */
+			$entries,  /* number of entries */
+			$centralsize,      /* size of the central directory */
+			$centraloffset,    /* offset of start of central dir */
+			0x0000);   /* ZIP comment length */
+}
---
+++ test.php
@@ -0,0 +1,65 @@
+<?php
+
+require 'ZippedHose.php';
+
+$list = array(
+	'/usr/home/saper/public_html/20100115',
+	'/usr/home/saper/public_html/404',
+	'/usr/home/saper/public_html/dump',
+	'/usr/home/saper/public_html/ExtraSettings.php',
+	'/usr/home/saper/public_html/fail',
+	'/usr/home/saper/public_html/gerrit-differ',
+	'/usr/home/saper/public_html/geshi.diff',
+	'/usr/home/saper/public_html/g.png',
+	'/usr/home/saper/public_html/h.html',
+	'/usr/home/saper/public_html/koalicja-spam.txt',
+	'/usr/home/saper/public_html/linkparser',
+	'/usr/home/saper/public_html/lovereq.log',
+	'/usr/home/saper/public_html/main',
+	'/usr/home/saper/public_html/mediawiki-1.15.1.tar.gz',
+	'/usr/home/saper/public_html/mediawiki-1.15.5.tar.gz',
+	'/usr/home/saper/public_html/mediawiki-1.18.1.tar.gz',
+	'/usr/home/saper/public_html/moving-to-branch',
+	'/usr/home/saper/public_html/mw115_1',
+	'/usr/home/saper/public_html/mw115_5',
+	'/usr/home/saper/public_html/mw118',
+	'/usr/home/saper/public_html/mwdumper-1.16.jar',
+	'/usr/home/saper/public_html/net',
+	'/usr/home/saper/public_html/oldpg',
+	'/usr/home/saper/public_html/opp',
+	'/usr/home/saper/public_html/opp2015.png',
+	'/usr/home/saper/public_html/pg',
+	'/usr/home/saper/public_html/pg0',
+	'/usr/home/saper/public_html/pg2',
+	'/usr/home/saper/public_html/pi.php',
+	'/usr/home/saper/public_html/Pollock_domena_publiczna.pdf',
+	'/usr/home/saper/public_html/ssh_svn.log',
+	'/usr/home/saper/public_html/system',
+	'/usr/home/saper/public_html/test',
+	'/usr/home/saper/public_html/testfile',
+	'/usr/home/saper/public_html/testfile.php',
+	'/usr/home/saper/public_html/trunk',
+	'/usr/home/saper/public_html/wf.diff',
+	'/usr/home/saper/public_html/widget',
+	'/usr/home/saper/public_html/w.png',
+	'/usr/home/saper/public_html/zipfile.php',
+	'/usr/home/saper/public_html/zipfile.zip'
+);
+
+$offset = 0;
+$centraloffset = 0;
+$centralsize = 0;
+$statlist = statfiles($list);
+foreach($statlist as $fstat) {
+	$lh = localheader($fstat, null);
+	$offset += strlen($lh);
+	print $lh;
+}
+
+$centraloffset = $offset;
+foreach($statlist as $fstat) {
+	$ch = centralheader($fstat, null);
+	$centralsize += strlen($ch);
+	print $ch;
+}
+print endcentral($centraloffset, $centralsize, count($statlist));
